6c01bf9bdbeef955dea7dd31b5e7e5f24da64a93
Jason Douglas <jdouglas71@gmail.com>
2012-05-01 13:07:42 -0700

    Reverted last change. I didn't do it the correct "git" way. I just copied over the last checkin. Sorry.

--- include/site-template.inc.php
+++ include/site-template.inc.php
@@ -135,7 +135,15 @@ class ThisSiteTemplate extends SiteTemplate {
 		<?php
 		if( $_SERVER['SERVER_NAME'] == 'www.careermaphumboldt.com' ) {
 		?>
-
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8726801-8");
+pageTracker._trackPageview();
+} catch(err) {}</script>
 		<?php
 		}
 
@@ -222,19 +230,7 @@ class ThisSiteTemplate extends SiteTemplate {
 			'help' => "Internal Help Text"
 		);
 	}
-	?>
-	
-	<script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	try {
-	var pageTracker = _gat._getTracker("UA-8726801-8");
-	pageTracker._trackPageview();
-	} catch(err) {}</script>
 
-<?php
 }
 
 ?>
