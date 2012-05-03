f612e035726aaf064364761040069de6086e5ec7
Effie Siverts <effie@sivecki.com>
2012-05-02 23:20:37 -0700

    updated Google Analytics code and location

--- include/site-template.inc.php
+++ include/site-template.inc.php
@@ -133,20 +133,6 @@ class ThisSiteTemplate extends SiteTemplate {
 		</ul>
 
 		<?php
-		if( $_SERVER['SERVER_NAME'] == 'www.careermaphumboldt.com' ) {
-		?>
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-8726801-8");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-		<?php
-		}
-
 	}
 
 	function HeaderScripts() {
@@ -181,7 +167,23 @@ pageTracker._trackPageview();
 		{
 			echo '<script type="text/javascript" src="' . $js. '"></script>'."\n";
 		}
+		?>
 
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', "<?=l('google analytics drawings')?>"]);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
+		
+		<?php
 	}
 
 	function tag_doctype() {
