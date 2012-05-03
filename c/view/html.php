533d542abaaccd9a4ca6a094769158973f7974bb
Effie Siverts <effie@sivecki.com>
2012-05-02 23:18:52 -0700

    updated Google Analytics code and location

--- c/view/html.php
+++ c/view/html.php
@@ -4,6 +4,19 @@
   <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <title><?= $drawing_name ?> &#8226; Career Pathways</title>
+    <script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', "<?=l('google analytics drawings')?>"]);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
   </head>
   <body>
 
@@ -16,16 +29,6 @@
 		<?php } ?>
 		Charts.draw();
 	</script>
-
-	<script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	try {
-	var pageTracker = _gat._getTracker("<?=l('google analytics drawings')?>");
-	pageTracker._trackPageview();
-	} catch(err) {}</script>
 
   </body>
 </html>
